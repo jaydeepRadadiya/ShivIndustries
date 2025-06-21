8efd2296785891ce109f62d1eb9f3ab0933ee826
[dyad] <git@dyad.sh>
2025-06-21 21:17:38 +0530

    [dyad] Adding logo to header - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,12 +5,15 @@ import { Link } from 'react-router-dom'; // Import Link
 const Header: React.FC = () => {
   return (
     <header className="w-full bg-black text-white shadow-lg py-4 px-6 flex justify-between items-center fixed top-0 left-0 right-0 z-50">
-      <div className="flex flex-col items-center">
-        <div className="text-2xl font-bold text-white">
-          SHIV INDUSTRIES
-        </div>
-        <div className="text-sm text-white uppercase text-center">
-          you ask we innovate
+      <div className="flex items-center"> {/* Changed to flex items-center for horizontal alignment */}
+        <img src="/Shiv-Industries Dark-.png" alt="Shiv Industries Logo" className="h-10 mr-3" /> {/* Added logo */}
+        <div className="flex flex-col items-start"> {/* Adjusted for text alignment */}
+          <div className="text-2xl font-bold text-white">
+            SHIV INDUSTRIES
+          </div>
+          <div className="text-sm text-white uppercase text-center">
+            you ask we innovate
+          </div>
         </div>
       </div>
       <nav className="hidden md:flex space-x-6">
