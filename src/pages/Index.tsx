c4ffe07d17a095b5475bbe3a12a82068b3e7a90f
[dyad] <git@dyad.sh>
2025-06-21 18:28:11 +0530

    Reverted all changes back to version 181e246ce14c530d8bd2cfb09f88ac67ba76426a

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import HeroSection from "@/components/sections/HeroSection";
 import WhyShivIndustry from "@/components/sections/WhyShivIndustry";
-import ProductRange from "@/components/sections/ProductRange";
 // Import other sections as they are created
 
 const Index: React.FC = () => {
@@ -13,7 +12,6 @@ const Index: React.FC = () => {
       <main className="flex-grow">
         <HeroSection />
         <WhyShivIndustry />
-        <ProductRange />
         {/* Other sections will be added here */}
       </main>
       <Footer />
