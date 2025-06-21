b42271945d73ee2ca9814378bf9d59205688d798
[dyad] <git@dyad.sh>
2025-06-21 20:27:48 +0530

    [dyad] Adjusted product card background color to be lighter - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,7 +10,7 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ imageSrc, title, description, benefit }) => {
   return (
-    <Card className="flex flex-col shadow-lg hover:shadow-xl transition-shadow duration-300 hover:-translate-y-1 bg-gray-900 border-gray-700">
+    <Card className="flex flex-col shadow-lg hover:shadow-xl transition-shadow duration-300 hover:-translate-y-1 bg-gray-800 border-gray-700">
       <img src={imageSrc} alt={title} className="w-full h-48 object-cover" />
       <CardHeader className="pb-2">
         <CardTitle className="text-xl font-semibold text-gray-200">{title}</CardTitle>
