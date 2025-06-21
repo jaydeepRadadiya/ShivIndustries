52947702d902efc8fa12667ba76f4e19829cf143
[dyad] <git@dyad.sh>
2025-06-21 21:23:32 +0530

    Reverted all changes back to version e854bee1e7ab71e1b75b9356f7805a33fa5c856b

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } => "@/components/ui/card";
 
 interface ProductCardProps {
   imageSrc: string;
@@ -11,7 +11,7 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ imageSrc, title, description, benefit }) => {
   return (
     <Card className="flex flex-col shadow-lg hover:shadow-xl transition-shadow duration-300 hover:-translate-y-1 bg-black border-gray-700">
-      <img src={imageSrc} alt={title} className="w-full h-48 object-contain p-4" />
+      <img src={imageSrc} alt={title} className="w-full h-48 object-contain p-4" /> {/* Added p-4 for padding */}
       <CardHeader className="pb-2">
         <CardTitle className="text-xl font-semibold text-white">{title}</CardTitle>
       </CardHeader>
